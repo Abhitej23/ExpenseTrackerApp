cf7d531b07d277136c32e0dfe20c4c7221ae102d
girish <girishnakate19@gmail.com>
2022-09-13 20:26:59 +0530

    first commit

--- backend/models/database.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize }= require("sequelize");
-
-const sequelize = new Sequelize("exdb", "root", "apple@17", {
-  dialect: "mysql",
-  host: "localhost",
-});
-
-module.exports = sequelize;
